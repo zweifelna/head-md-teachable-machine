f5d0db0ea772741a6c0db2cc2347f9750e1ae62a
Luka Najjar <46554723+fly04@users.noreply.github.com>
2023-03-01 17:31:52 +0100

    gameplay skeleton

--- classes/Note.js
+++ classes/Note.js
@@ -3,10 +3,43 @@ class Note {
         this.position = createVector(-radius / 2, height * .65);
         this.speed = speed;
         this.radius = radius;
+        this.type = Math.floor(Math.random() * 3);  //0 → pierre, 1 → feuille, 2 → ciseaux
+        this.isActive = false;
     }
 
     update() {
         this.position.x += this.speed;
+        if (this.isActive) {
+            fill(color(255, 0, 0));
+        } else {
+            fill(color(255, 255, 255));
+        }
         circle(this.position.x, this.position.y, this.radius);
+
+        //resize image
+        rock.resize(this.radius, this.radius);
+        paper.resize(this.radius, this.radius);
+        scissors.resize(this.radius, this.radius);
+
+        switch (this.type) {
+            case 0:
+                image(rock, this.position.x, this.position.y);
+                break;
+            case 1:
+                image(paper, this.position.x, this.position.y);
+                break;
+            case 2:
+                image(scissors, this.position.x, this.position.y);
+                break;
+        }
+    }
+
+    isInPerfectZone() {
+        return (boxCenter.x - boxRadius / 2 < this.position.x && this.position.x < boxCenter.x + boxRadius / 2);
+
+    }
+
+    isInOkZone() {
+        return (boxCenter.x - boxRadius / 2 - this.radius / 2 < this.position.x && this.position.x < boxCenter.x + boxRadius / 2 + this.radius / 2);
     }
 }
--- sketch.js
+++ sketch.js
@@ -3,6 +3,10 @@
 let sound;
 let conductor;
 
+let rock;
+let paper;
+let scissors;
+
 // Game state
 let isPlaying = false;
 let inGame = false;
@@ -15,8 +19,9 @@ let boxRadius = 200;
 // Notes stuff
 let offset;
 let notes = [];
-const noteSpeed = 15;
+const noteSpeed = 12;
 const noteRadius = 160;
+let activeNote;
 
 // Timer
 let countdown;
@@ -28,15 +33,22 @@ let startButton;
 
 function preload() {
     sound = new Howl({
-        src: ['./margot.mp3'],
+        src: ['./assets/margot.mp3'],
         onend: function () { isPlaying = false; }
     })
+
+    rock = loadImage('assets/rock.png');
+    paper = loadImage('assets/paper.png');
+    scissors = loadImage('assets/scissors.png');
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     rectMode(CENTER);
+    ellipseMode(CENTER);
     textAlign(CENTER);
+    imageMode(CENTER);
+    frameRate(60);
 
     /*TITLE SCREEN*/
     // Start button
@@ -50,11 +62,6 @@ function setup() {
 
     // Notes
     offset = (width / 2 + noteRadius / 2) / noteSpeed;
-
-    //Timer 
-    timer = createDiv('');
-    // timer.style('font-size', '64px');
-    timer.position(width / 2, height / 2);
 }
 
 function draw() {
@@ -63,18 +70,40 @@ function draw() {
 
         /*GAME*/
         // Box
+        fill(color(255, 255, 255));
         square(boxCenter.x, boxCenter.y, boxRadius);
+        line(width / 2, 0, width / 2, height);
 
         if (conductor !== undefined) {
             if (conductor.shouldSpawnNote()) {
-                notes.push(new Note({ speed: noteSpeed, radius: noteRadius }));
+                let newNote = new Note({ speed: noteSpeed, radius: noteRadius });
+                notes.push(newNote);
             }
         }
 
         notes.forEach(note => {
             note.update();
+            if (activeNote == note) {
+                note.isActive = true;
+            } else {
+                note.isActive = false;
+            }
+
+            // if (note.position.x > width + note.radius) {
+            //     notes.shift();          // pourquoi stutter?
+            // }
         });
+
+        if (notes.length == 1) {
+            activeNote = notes[0];
+        }
+
+        if (notes.length > 0) {
+            setActiveNote();
+        }
     }
+
+    // console.log(frameRate());
 }
 
 let start = () => {
@@ -84,36 +113,44 @@ let start = () => {
     // Destroy the startButton
     startButton.remove();
 
-    // Start the countdown
-    startCountdown();
-
     // Wait 1 second then start the game
     setTimeout(() => {
         // Start the music
         sound.play();
 
         // Start the conductor
-        conductor = new Conductor(120);
+        conductor = new Conductor(70);
         conductor.start(offset);
     }, 1000);
 }
 
-let startCountdown = () => {
-    countdown = 3;
-    timer.html(countdown);
-    const interval = setInterval(() => {
-        countdown--;
-        if (countdown > 0) {
-            timer.html(countdown);
-        } else if (countdown === 0) {
-            timer.html('GO!');
-        } else {
-            // timer.hide();
-            clearInterval(interval);
-        }
-    }, 1000 / 3);
-}
 
+
+let setActiveNote = () => {
+    notes.forEach(note => {
+        // if (note.position.x > boxCenter.x + boxRadius / 2 + note.radius / 2) {
+        //     notes.shift();
+        // }
+
+
+        // if (notes[notes.indexOf(note) + 1] == undefined) {
+        //     console.log('return');
+        //     return;
+        // } else {
+        //     console.log('pas return');
+        // }
+        // if (note.position.x < boxCenter.x + boxRadius / 2 + note.radius / 2
+        //     && notes.indexOf(note) < notes.indexOf()
+        // ) {
+        //     activeNote = note;
+        // }
+
+
+        if (activeNote.position.x > boxCenter.x + boxRadius / 2 + noteRadius / 2) {
+            activeNote = notes[notes.indexOf(activeNote) + 1];
+        }
+    });
+}
 
 //quand label == ce qu'on veut → incrémenter un counter
 //quand counter > counterthreshold → prendre en compte l'input + reset counter
